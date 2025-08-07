6a64ffbcc45c0e6557441cf18eeeb0844f67973a
Vishruth V Shelkey <vishruthvshelkey@Vishruths-MacBook-Pro.local>
2025-08-07 14:22:45 +0530

    updated folder

--- Program_Files/drawing_or_filling/drawing_rectangle.py
+++ Program_Files/drawing_or_filling/drawing_rectangle.py
@@ -2,3 +2,16 @@ import cv2 as cv
 import numpy as np
 
 blank = np.zeros((500, 500, 3), dtype="uint8")
+
+# Drawing a rectangle
+cv.rectangle(blank, (1, 1), (250, 250), (0, 255, 0), thickness=2)
+cv.imshow("Rectangle", blank)
+
+
+# Drawing a filled rectangle
+cv.rectangle(
+    blank, (1, 1), (400, 400), (0, 255, 0), thickness=cv.FILLED
+)  # alternative: thickness=-1
+cv.imshow("Filled Rectangle", blank)
+
+cv.waitKey(0)
